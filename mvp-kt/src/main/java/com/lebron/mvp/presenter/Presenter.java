f25c745890de1827056f558f5290cf8769278db4
lebron <worldpeace370@163.com>
2017-01-02 01:02:47 +0800

    增加mvp library

---
+++ mvp-kt/src/main/java/com/lebron/mvp/presenter/Presenter.java
@@ -0,0 +1,8 @@
+package com.lebron.mvp.presenter;
+
+/**
+ * Created by wuxia on 2017/1/2.
+ */
+
+public class Presenter {
+}
---
+++ mvp-kt/src/main/java/com/lebron/mvp/view/LebronMvpFragment.java
@@ -0,0 +1,10 @@
+package com.lebron.mvp.view;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by wuxia on 2017/1/2.
+ */
+
+public class LebronMvpFragment extends Fragment {
+}
