602c02c2ea777bd0d0378aee9ce3ec2b88d98e09
lebron <worldpeace370@163.com>
2017-01-29 23:32:57 +0800

    增加日历选择的逻辑,进行了MVP的实现.下一步绘制折线部分也需要MVP实现

--- app/src/main/java/com/lebron/graduationpro1/scanpage/presenter/ScanPresenter.java
+++ app/src/main/java/com/lebron/graduationpro1/scanpage/presenter/ScanPresenter.java
@@ -3,9 +3,17 @@ package com.lebron.graduationpro1.scanpage.presenter;
 import com.lebron.graduationpro1.scanpage.contracts.ScanContracts;
 import com.lebron.graduationpro1.scanpage.model.CollectInfoBean;
 import com.lebron.graduationpro1.scanpage.model.ScanRequestModel;
+import com.lebron.graduationpro1.view.customcalendarview.CalendarTools;
 import com.lebron.mvp.presenter.Presenter;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import static com.lebron.graduationpro1.scanpage.view.ScanFragment.POSITION_FIRST_DAY;
+import static com.lebron.graduationpro1.scanpage.view.ScanFragment.POSITION_SECOND_DAY;
+import static com.lebron.graduationpro1.scanpage.view.ScanFragment.POSITION_THIRD_DAY;
+import static java.lang.System.currentTimeMillis;
 
 /**
  * Created by wuxiangkun on 2017/1/14.
@@ -13,8 +21,11 @@ import java.util.List;
  */
 
 public class ScanPresenter extends Presenter<ScanContracts.View> implements
-        ScanContracts.Presenter, ScanContracts.Model.Callback{
+        ScanContracts.Presenter, ScanContracts.Model.Callback {
     private ScanRequestModel mRequestModel;
+    private int mTodayYear, mTodayMonth, mTodayDay; //今天的年月日
+    private int mSelectYear, mSelectMonth, mSelectDay; //当前通过日历选中的年月日
+    private int mThirdYear, mThirdMonth, mThirdDay; //顶部的第三天的年月日
 
     public ScanPresenter() {
         mRequestModel = new ScanRequestModel();
@@ -24,14 +35,128 @@ public class ScanPresenter extends Presenter<ScanContracts.View> implements
     @Override
     public void getCollectInfo(String date) {
         if (getView() != null) {
-            getView().showLoading();
-            mRequestModel.getCollectInfo(date);
+            //            getView().showLoading();
+            mRequestModel.getCollectInfo(date);//请求数据,然后回调到onSuccess中,在这里面更新UI
+        }
+    }
+
+    @Override
+    public void initTodayData() {
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date(currentTimeMillis());
+        calendar.clear();
+        calendar.setTime(date);
+
+        mTodayYear = calendar.get(Calendar.YEAR);
+        mTodayMonth = calendar.get(Calendar.MONTH);
+        mTodayDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        mSelectYear = mTodayYear;
+        mSelectMonth = mTodayMonth;
+        mSelectDay = mTodayDay;
+
+        mThirdYear = mTodayYear;
+        mThirdMonth = mTodayMonth;
+        mThirdDay = mTodayDay;
+
+        if (getView() != null) {
+            getView().changeTopDateColor(POSITION_THIRD_DAY);
+            getView().setTopDateLayout(mTodayYear, mTodayMonth, mTodayDay);
+            getCollectInfo(CalendarTools.dateToString(mTodayYear, mTodayMonth, mTodayDay));
+        }
+    }
+
+    @Override
+    public void changeToFirstPositionDay() {
+        Calendar calendar = CalendarTools.createCalendar(mThirdYear, mThirdMonth, mThirdDay);
+        calendar.add(Calendar.DATE, -2);
+        mSelectYear = calendar.get(Calendar.YEAR);
+        mSelectMonth = calendar.get(Calendar.MONTH);
+        mSelectDay = calendar.get(Calendar.DAY_OF_MONTH);
+        if (getView() != null) {
+            getView().changeTopDateColor(POSITION_FIRST_DAY);
+            getCollectInfo(CalendarTools.dateToString(mSelectYear, mSelectMonth, mSelectDay));
+        }
+    }
+
+    @Override
+    public void changeToSecondPositionDay() {
+        Calendar calendar = CalendarTools.createCalendar(mThirdYear, mThirdMonth, mThirdDay);
+        calendar.add(Calendar.DATE, -1);
+        mSelectYear = calendar.get(Calendar.YEAR);
+        mSelectMonth = calendar.get(Calendar.MONTH);
+        mSelectDay = calendar.get(Calendar.DAY_OF_MONTH);
+        if (getView() != null) {
+            getView().changeTopDateColor(POSITION_SECOND_DAY);
+            getCollectInfo(CalendarTools.dateToString(mSelectYear, mSelectMonth, mSelectDay));
+        }
+    }
+
+    @Override
+    public void changeToThirdPositionDay() {
+        mSelectYear = mThirdYear;
+        mSelectMonth = mThirdMonth;
+        mSelectDay = mThirdDay;
+        if (getView() != null) {
+            getView().changeTopDateColor(POSITION_THIRD_DAY);
+            getCollectInfo(CalendarTools.dateToString(mSelectYear, mSelectMonth, mSelectDay));
+        }
+    }
+
+    @Override
+    public void changeToSelectDay(Calendar calendar) {
+        mSelectYear = calendar.get(Calendar.YEAR);
+        mSelectMonth = calendar.get(Calendar.MONTH);
+        mSelectDay = calendar.get(Calendar.DAY_OF_MONTH);
+        mThirdYear = mSelectYear;
+        mThirdMonth = mSelectMonth;
+        mThirdYear = mSelectDay;
+        if (getView() != null) {
+            getView().changeTopDateColor(POSITION_THIRD_DAY);
+            getView().setTopDateLayout(mThirdYear, mThirdMonth, mThirdYear);
+            getCollectInfo(CalendarTools.dateToString(mSelectYear, mSelectMonth, mSelectDay));
+        }
+    }
+
+    @Override
+    public void setBackTodayVisibility(Calendar calendar) {
+        if (getView() != null) {
+            if (calendar.get(Calendar.YEAR) != mTodayYear || calendar.get(Calendar.MONTH) != mTodayMonth) {
+                getView().setBackTodayVisibility(true);
+            } else {
+                getView().setBackTodayVisibility(false);
+            }
+        }
+    }
+
+    @Override
+    public void backToToday() {
+        if (getView() != null) {
+            getView().backToToday(mTodayYear, mTodayMonth, mTodayDay);
+        }
+    }
+
+    @Override
+    public void showSelectDateWindow() {
+        if (getView() != null) {
+            getView().showSelectDateWindow(mSelectYear, mSelectMonth, mSelectDay);
+            if (mSelectYear != mTodayYear || mSelectMonth != mTodayMonth) {
+                getView().setBackTodayVisibility(true);
+            } else {
+                getView().setBackTodayVisibility(false);
+            }
         }
     }
 
     @Override
     public void onSuccess(List<CollectInfoBean> infoList) {
-
+        if (getView() != null) {
+            if (infoList.isEmpty()) {
+                getView().showEmpty();
+            } else {
+                getView().showContent(infoList);
+            }
+        }
     }
 
     @Override
