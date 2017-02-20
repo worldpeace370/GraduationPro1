b90f769530dfee6f559ba54b7f801b27b22c40b9
lebron <worldpeace370@163.com>
2017-02-20 13:58:00 +0800

    设计了网络接口请求

--- app/src/main/java/com/lebron/graduationpro1/scanpage/model/CollectInfoBean.java
+++ app/src/main/java/com/lebron/graduationpro1/scanpage/model/CollectInfoBean.java
@@ -6,5 +6,37 @@ package com.lebron.graduationpro1.scanpage.model;
  */
 
 public class CollectInfoBean {
+    private String mTemperature;
+    private String mRate;
+    private String mCreateTime;
 
+    public String getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(String temperature) {
+        mTemperature = temperature;
+    }
+
+    public String getRate() {
+        return mRate;
+    }
+
+    public void setRate(String rate) {
+        mRate = rate;
+    }
+
+    public String getCreateTime() {
+        return mCreateTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        mCreateTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "mTemperature = " + mTemperature + ", mRate = " + mRate
+                + ", mCreateTime = " + mCreateTime;
+    }
 }
---
+++ app/src/main/java/com/lebron/graduationpro1/service/VolleyService.java
@@ -0,0 +1,40 @@
+package com.lebron.graduationpro1.service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.lebron.graduationpro1.base.AppApplication;
+
+import java.util.List;
+
+/**
+ * Created by wuxiangkun on 2017/2/20.
+ * Contact way wuxiangkun2015@163.com
+ */
+
+public class VolleyService {
+    public <T> void getDataFromServer(String urlStr, final Class<T> type, final RequestCompleteListener<T> listener) {
+        StringRequest request = new StringRequest(urlStr, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.equals("")) {
+                    List<T> list = JSONArray.parseArray(response, type);
+                    listener.success(list);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.error(error.getMessage());
+            }
+        });
+        AppApplication.getRequestQueue().add(request);
+    }
+
+    public interface RequestCompleteListener<T> {
+        void success(List<T> list);
+
+        void error(String error);
+    }
+}
